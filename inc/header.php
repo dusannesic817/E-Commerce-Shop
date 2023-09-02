9f4f53c8a6938e385e954ef513bb5dba6e7be1a9
dusannesic817 <dusannesic28@gmail.com>
2023-09-02 16:08:07 +0200

    header

---
+++ inc/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width,initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.css" integrity="sha512-KOWhIs2d8WrPgR4lTaFgxI35LLOp5PRki/DxQvb7mlP29YZ5iJ5v8tiLWF7JLk5nDBlgPP1gHzw96cZ77oD7zQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <title>Header</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg navbar_color">
+  <div class="container-fluid">
+  <a class="navbar-brand navbar-text" href="index.php">Premier
+      <img src="../public/images/logo1.png" alt="Logo" width="50" height="35" class="d-inline-block align-text-top">
+        League 
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link navbar-text" aria-current="page" href="cart.php"><i class="bi bi-bag-fill margin_cart"></i> Cart</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link navbar-text" href="login.php"><i class="bi bi-person-fill margin_cart"></i> Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link navbar-text " href="register.php"><i class="bi bi-person-fill-gear margin_cart"></i> Register</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</body>
+</html>

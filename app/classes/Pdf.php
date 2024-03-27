771a48fc430213be8119a04a2c06d420d9f5fa7b
dusannesic817 <dusannesic28@gmail.com>
2024-03-28 00:37:39 +0100

    pdf

--- app/classes/Pdf.php
+++ app/classes/Pdf.php
@@ -9,36 +9,34 @@ class Pdf{
     public function generatePdf_forOrders($order_id,$first_name,$last_name,$address,$state,$email,$niz,$totalPrice,$filename){
         $pdf = new FPDF();
 
-        // Dodajte prvu stranicu
+
         $pdf->AddPage();
         
-        // Postavite font
+       
         $pdf->SetFont('Arial','B',12);
-        // Prikaz naslova
+     
         $pdf->Cell(0,10,'#'. $order_id.' order',0,1,'C');
-        // Linija ispod naslova
+     
         $pdf->Cell(0,0,'','B',1,'C');
      
         $pdf->Ln(10);
         
-        // Postavljanje fonta i veličine za opis
+  
         $pdf->SetFont('Arial','',10);
         
-        // Ispisivanje podataka
+
         $pdf->Cell(0,10,'Name: '.$first_name.' '.$last_name,0,1);
         $pdf->Cell(0,10,'Address: '. $address,0,1);
         $pdf->Cell(0,10,'State: '.$state,0,1);
         $pdf->Cell(0,10,'Email: '.$email,0,1);
         
-        // Ispisivanje slike
+      
         
-        //$pdf->Image('putanja/do/slike.jpg',10,$pdf->GetY(),30);
+        //$pdf->Image('image/image.jpg',10,$pdf->GetY(),30);
         $pdf->Ln(10);
 
         foreach ($niz as $value){
 
-        
-        // Linija ispod cene
         $pdf->Cell(0,0,'','T',1,'C');
         $pdf->Cell(0,10,'Name of artical: '.$value['name'],0,1);
         $pdf->Cell(0,10,'Price: '.$value['price'] .'$',0,1);
